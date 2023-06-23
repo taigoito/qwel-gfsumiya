fed644f839a2d9c47d96d3e0621751e7a08f9854
Taigo Ito <webmaster@qwel.design>
2023-06-23 14:08:55 +0900

    Fix header and use Google font

--- classes/Scripts.php
+++ classes/Scripts.php
@@ -7,6 +7,12 @@ trait Scripts {
     // バージョン情報を取得
 		$version = wp_get_theme()->get( 'Version' );
 
+		// Google Fonts
+    $family = 'M+PLUS+Rounded+1c:wght@400;700';
+    if ($family !== '') {
+      wp_enqueue_style('fonts', 'https://fonts.googleapis.com/css?family=' . $family . '&display=swap', [], null); 
+    }
+
 		// style.css
 		wp_enqueue_style(
 			'style',
